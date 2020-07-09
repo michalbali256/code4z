7dfc9b68beca4421c2ad1cc8ec23ad90ffc4bc09
Michal Bali <michalbali32@gmail.com>
2020-07-09 17:15:08 +0200

    feat: push to master

--- prepare_release.sh
+++ prepare_release.sh
@@ -6,7 +6,7 @@ RELEASE_NOTES=$3
 OLD_VERSION=$4
 DATE=$5
 
-# { printf "%s" "$RELEASE_NOTES"; cat ../../CHANGELOG.md; } > tmp.md
-#mv tmp.md ../../CHANGELOG.md
+{ printf "%s" "$RELEASE_NOTES"; cat CHANGELOG.md; } > tmp.md
+mv tmp.md CHANGELOG.md
 
 sed -i 's/"version": ".*"/"version": "'$VERSION'"/g' package.json
